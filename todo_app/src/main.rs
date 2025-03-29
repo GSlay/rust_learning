647c08270d32e3974b4ee9e30e21737c54cfdfde
GSlay <mha220871@gmail.com>
2025-03-29 07:19:13 +0000

    temp update

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -1,5 +1,17 @@
 mod task;
+use crate::task_list::TaskList;
+use std::io;
+mod task_list;
+
+const task_list: TaskList = TaskList::new();
 
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    while input != "exit" {
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed read_line()");
+    }
 }
+
+fn input_to_task(input: String) -> () {}
